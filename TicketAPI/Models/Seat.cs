46568415e6d7b4f291f308ee265116c263c47fb5
Fausto Gomez <gomez_fausto@hotmail.com>
2020-02-04 12:32:56 -0800

    Add project files.

---
+++ TicketAPI/Models/Seat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TicketAPI.Models
+{
+    public partial class Seat
+    {
+        public Seat()
+        {
+            EventSeat = new HashSet<EventSeat>();
+        }
+
+        public int SeatId { get; set; }
+        public decimal? Price { get; set; }
+        public int? RowId { get; set; }
+
+        public virtual Row Row { get; set; }
+        public virtual ICollection<EventSeat> EventSeat { get; set; }
+    }
+}
